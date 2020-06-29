2876debab78e7af039615eda7866921fc26e3a36
Nikhil Prabhu <nikhilprabhu00@gmail.com>
2020-06-29 23:23:27 +0530

    Create basic UI.
    	This commit creates a basic main window user interface for
    	the application and sets the window title to the name of
    	the application.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define APPLICATION_NAME "QNotepad"     // Application name
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->setWindowTitle(APPLICATION_NAME);
 }
 
 MainWindow::~MainWindow()
