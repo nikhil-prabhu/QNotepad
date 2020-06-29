23ed0186aafd3b712b5b01956f0978edda7e3367
Nikhil Prabhu <nikhilprabhu00@gmail.com>
2020-06-29 23:32:12 +0530

    Add quit menu action functionality.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+
 #define APPLICATION_NAME "QNotepad"     // Application name
 
 MainWindow::MainWindow(QWidget *parent)
@@ -10,6 +12,19 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     this->setWindowTitle(APPLICATION_NAME);
+}
+
+/* MENU ACTIONS */
+
+// Quit action
+void MainWindow::on_actionQuit_triggered()
+{
+    auto userChoice = QMessageBox::question(this, "Quit QNotepad", "Are you sure you want to quit?");
+
+    if ( userChoice == QMessageBox::Yes )
+    {
+        close();        // Quit the application
+    }
 }
 
 MainWindow::~MainWindow()
